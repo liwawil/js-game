32c3f803d312c16512a43bfb33ee7dc05e30ddf3
liwawil <liwa.johnson@gmail.com>
2024-11-24 10:56:28 -0500

    add movement for player

--- index.js
+++ index.js
@@ -6,13 +6,36 @@ canvas.height = 64 * 9;
 
 const player = new Player();
 
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+}
+
 function animate() {
     window.requestAnimationFrame(animate);
     ctx.fillStyle = 'white';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+    player.velocity.x = 0
+    if (keys.d.pressed) {
+        player.velocity.x = 5;
+    } else if (keys.a.pressed) {
+        player.velocity.x = -5;
+    }
 
     player.draw();
     player.update();
 }
 
 animate();
+
